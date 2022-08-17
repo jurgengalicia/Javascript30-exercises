36d304079adf1e1e2473f46b404efbc8a665edcc
jurgengalicia <jurgen.galicia@gmail.com>
2022-08-17 00:12:45 -0700

    exercise 1 complete

--- 01 - JavaScript Drum Kit/scripts.js
+++ 01 - JavaScript Drum Kit/scripts.js
@@ -1,8 +1,22 @@
 function playKey(e){
-    let pressedAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const pressedAudio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const pressedKey = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     if(!pressedAudio) return;
-    else pressedAudio.play();
+
+    pressedAudio.currentTime = 0;
+    pressedAudio.play();
+
+    pressedKey.classList.add("playing");
+}
+
+function removeTransition(e){
+    if(e.propertyName !== "transform") return;
+    console.log(e.propertyName);
+    this.classList.remove("playing")
 }
 
 
 window.addEventListener("keydown", playKey)
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend",removeTransition))
